4838fb1797189da1d026b1c7e20fb027ad22f8e7
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2018-07-12 16:19:58 +0200

    Documented Bottle API

--- Python/bottle/main.py
+++ Python/bottle/main.py
@@ -6,18 +6,37 @@ from bottle import abort, request
 
 from database import Database
 
+# initializing the application as a default bottle application
 app = application = bottle.default_app()
 
 
 @get('/')
 def greetings():
+    """basic entrypoint
+
+    Basic app's entrypoint. Greets the user with its framework
+    """
     return 'Hello from Bottle !'
 
 
 @get('/users/<user_id:int>')
 def get_user(user_id: int):
+    """show user's information
+
+    Given an ID, retrieve user's information
+
+    Args:
+        user_id (int): the id of the user we are
+            looking for in the database
+
+    Returns:
+        JSON: the user's information with ``name `` and ``age``
+            as keys.
+    """
+    # retrieving records
     records = Database.get_user(user_id)
 
+    # establish response given ``records`` state
     if not records:
         response = {'message': 'no user found'}
     else:
@@ -32,10 +51,22 @@ def get_user(user_id: int):
 
 @post('/users')
 def add_user():
+    """add a user to the database
+
+    From a JSON payload, create a new record in the database
+    Payload should provide relevant information, which are:
+        - name as a string
+        - age as an integer
+
+    Returns:
+        JSON: ``{'message': 'success'}`on success
+    """
+    # ensure that the request is correct and contains relevant keys
     if not request.json \
             or not all(k in request.json for k in ['name', 'age']):
         abort(400)
 
+    # create the new record
     Database.add_user(
         name=request.json['name'],
         age=request.json['age']
@@ -46,5 +77,8 @@ def add_user():
 
 
 if __name__ == '__main__':
+    # create the database
     Database.create_all()
+
+    # launch the app on port 8080
     bottle.run(port=8080)
